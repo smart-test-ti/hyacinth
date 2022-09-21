41a9d22e22f1c3f775572d0371ec5b5858c7139a
Lijiawei <1456470136@qq.com>
2022-09-21 17:39:29 +0800

    fix: cannot import name 'url' from 'django.conf.urls'
    https://github.com/smart-test-ti/hyacinth/issues/1

--- hyacinth/urls.py
+++ hyacinth/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views import static
 from django.conf import settings
-from django.conf.urls import url
+from django.urls import re_path as url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
